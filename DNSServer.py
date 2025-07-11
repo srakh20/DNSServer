c817b8d5a12aa5a6989a8ad5bfe58cef6a02d166
Sarah R <sarah.rakhamimov@baruchmail.cuny.edu>
2025-07-11 19:08:59 -0400

    dns server changes

--- DNSServer.py
+++ DNSServer.py
@@ -150,7 +150,7 @@ dns_records = {
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],  # List of (preference, mail server) tuples
         dns.rdatatype.CNAME: 'www.nyu.edu.',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
-        dns.rdatatype.TXT: (encrypted_value,),
+        dns.rdatatype.TXT: (str(encrypted_value),),
         dns.rdatatype.SOA: (
             'ns1.nyu.edu.',  # mname
             'admin.nyu.edu.',  # rname
